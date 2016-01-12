d0b36bb652490d7cc8876a205892119ee4145f9c
Sven Baumann <baumann.sv@gmail.com>
2016-01-12 13:45:50 +0100

    reform code

--- src/Avisota/Recipient/Fake/FakeRecipient.php
+++ src/Avisota/Recipient/Fake/FakeRecipient.php
@@ -24,133 +24,133 @@ use Faker\Factory;
 class FakeRecipient extends MutableRecipient
 {
 
-	/**
-	 * @var null
-	 */
-	protected $locale = null;
+    /**
+     * @var null
+     */
+    protected $locale = null;
 
-	/**
-	 * The generators seed.
-	 *
-	 * @var null|string
-	 */
-	protected $seed = null;
+    /**
+     * The generators seed.
+     *
+     * @var null|string
+     */
+    protected $seed = null;
 
-	/**
-	 * The fake data.
-	 *
-	 * @var array
-	 */
-	protected $data = null;
+    /**
+     * The fake data.
+     *
+     * @var array
+     */
+    protected $data = null;
 
-	/**
-	 * @param       $email
-	 * @param array $details
-	 */
-	public function __construct($locale = null, $seed = null)
-	{
-		$this->locale = $locale;
-		$this->seed   = $seed;
-	}
+    /**
+     * @param       $email
+     * @param array $details
+     */
+    public function __construct($locale = null, $seed = null)
+    {
+        $this->locale = $locale;
+        $this->seed   = $seed;
+    }
 
-	protected function init()
-	{
-		if ($this->data !== null) {
-			return;
-		}
+    protected function init()
+    {
+        if ($this->data !== null) {
+            return;
+        }
 
-		$faker = Factory::create($this->locale ? : Factory::DEFAULT_LOCALE);
+        $faker = Factory::create($this->locale ?: Factory::DEFAULT_LOCALE);
 
-		if ($this->seed !== null) {
-			$faker->seed($this->seed);
-		}
+        if ($this->seed !== null) {
+            $faker->seed($this->seed);
+        }
 
-		$gender = rand(0, 1) ? 'male' : 'female';
+        $gender = rand(0, 1) ? 'male' : 'female';
 
-		$this->data = array(
-			'email'    => $faker->email,
-			'forename' => $faker->format('firstName', array($gender)),
-			'surname'  => $faker->format('lastName', array($gender)),
-			'gender'   => $gender,
-			'company'  => $faker->company,
-			'street'   => $faker->streetAddress,
-			'postal'   => $faker->postcode,
-			'city'     => $faker->city,
-			'country'  => $faker->country,
-		);
-	}
+        $this->data = array(
+            'email'    => $faker->email,
+            'forename' => $faker->format('firstName', array($gender)),
+            'surname'  => $faker->format('lastName', array($gender)),
+            'gender'   => $gender,
+            'company'  => $faker->company,
+            'street'   => $faker->streetAddress,
+            'postal'   => $faker->postcode,
+            'city'     => $faker->city,
+            'country'  => $faker->country,
+        );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getEmail()
-	{
-		$this->init();
-		return $this->data['email'];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getEmail()
+    {
+        $this->init();
+        return $this->data['email'];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function setEmail($email)
-	{
-		$this->init();
-		parent::setEmail($email);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function setEmail($email)
+    {
+        $this->init();
+        parent::setEmail($email);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function hasDetails()
-	{
-		return true;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function hasDetails()
+    {
+        return true;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function get($name)
-	{
-		$this->init();
-		if (array_key_exists($name, $this->data)) {
-			return $this->data[$name];
-		}
-		return null;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function get($name)
+    {
+        $this->init();
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+        return null;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function set($name, $value)
-	{
-		$this->init();
-		parent::set($name, $value);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function set($name, $value)
+    {
+        $this->init();
+        parent::set($name, $value);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getDetails()
-	{
-		$this->init();
-		return $this->data;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getDetails()
+    {
+        $this->init();
+        return $this->data;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function setDetails(array $details)
-	{
-		$this->init();
-		parent::setDetails($details);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function setDetails(array $details)
+    {
+        $this->init();
+        parent::setDetails($details);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getKeys()
-	{
-		$this->init();
-		return array_keys($this->data);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getKeys()
+    {
+        $this->init();
+        return array_keys($this->data);
+    }
 }
